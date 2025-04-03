8bdefc152c19c1c655a83169889173231859c1b3
Tawfique Elahey <177960094+tawfique02@users.noreply.github.com>
2025-04-03 10:34:51 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,7 +40,6 @@ pkg install wget -y
 echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
 echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 termux-setup-storage
-cp login.sh $PREFIX/etc
 # Prompt user for update
 read -p "Do you want to update the repository now? (y/n): " choice
 if [ "$choice" = "y" ]; then
